567e053f9157c30009a1bf937e1275da9823fe85
Alzatiyo <140862803+Alzatiyo@users.noreply.github.com>
2026-05-20 16:37:54 -0500

    Delete Infrastructure directory

--- Infrastructure/Dtos/RegisterDeliveryRequest.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Infrastructure.Dtos;
-
-public class RegisterDeliveryRequest
-{
-    public string ProofImageUrl { get; set; } = string.Empty;
-}
--- Infrastructure/Dtos/UpdateFlightStatusRequest.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Infrastructure.Dtos;
-
-public class UpdateFlightStatusRequest
-{
-    public string Status { get; set; } = string.Empty;
-}
